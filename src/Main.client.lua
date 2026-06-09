b6d50cbd0ab642a88bed6ef2381b5daf207b6cc2
tomatizuo-dot <tomatizuo@gmail.com>
2026-06-10 00:19:49 +0900

    Update Main.client.lua

--- src/Main.client.lua
+++ src/Main.client.lua
@@ -1,11 +1,13 @@
+local Renderer = require(script.Parent.UI.Renderer)
+
 local Categories = {
-    require(script.Parent.Categories.Combat),
-    require(script.Parent.Categories.Movement),
-    require(script.Parent.Categories.Render),
-    require(script.Parent.Categories.Misc),
-    require(script.Parent.Categories.Settings)
+	require(script.Parent.Categories.Combat),
+	require(script.Parent.Categories.Movement),
+	require(script.Parent.Categories.Render),
+	require(script.Parent.Categories.Misc),
+	require(script.Parent.Categories.Settings)
 }
 
-for _, Category in ipairs(Categories) do
-    print("Loading:", Category.Name)
-end
+local Windows = Renderer:Render(Categories)
+
+print("Loaded", #Windows, "windows")
