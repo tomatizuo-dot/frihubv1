1b5f1fc0c7aa757a9724bb688e4411bd01b030f9
tomatizuo-dot <tomatizuo@gmail.com>
2026-06-10 00:03:58 +0900

    Update Module.lua

--- src/UI/Module.lua
+++ src/UI/Module.lua
@@ -1,1 +1,13 @@
+local Module = {}
 
+function Module.new(Name)
+    local Self = {}
+
+    Self.Name = Name
+    Self.Enabled = false
+    Self.Expanded = false
+
+    return Self
+end
+
+return Module
