7d63798c425be05363fa001aa52643215ba074f2
tomatizuo-dot <tomatizuo@gmail.com>
2026-06-10 00:06:39 +0900

    Update Window.lua

--- src/UI/Window.lua
+++ src/UI/Window.lua
@@ -1,12 +1,23 @@
+local Module = require(script.Parent.Module)
+
 local Window = {}
+Window.__index = Window
 
 function Window.new(Name)
-    local Self = {}
+    local Self = setmetatable({}, Window)
 
     Self.Name = Name
     Self.Modules = {}
 
     return Self
+end
+
+function Window:AddModule(Name)
+    local NewModule = Module.new(Name)
+
+    table.insert(self.Modules, NewModule)
+
+    return NewModule
 end
 
 return Window
