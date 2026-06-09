6ba287fc31dc32de9f1664e3e42893ad8d90c05a
tomatizuo-dot <tomatizuo@gmail.com>
2026-06-10 00:33:47 +0900

    Update Main.client.lua

--- src/Main.client.lua
+++ src/Main.client.lua
@@ -8,18 +8,4 @@ local Categories = {
 	require(script.Parent.Categories.Settings)
 }
 
-local Windows = Renderer:Render(Categories)
-
-print("Loaded Windows:", #Windows)
-
-for _, Window in ipairs(Windows) do
-	print(Window.Name)
-
-	for _, Module in ipairs(Window:GetModules()) do
-		print("  ->", Module.Name)
-
-		for SettingName, SettingValue in pairs(Module:GetSettings()) do
-			print("     ", SettingName, SettingValue)
-		end
-	end
-end
+Renderer:Render(Categories)
