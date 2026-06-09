90ea26f1fee4969b825029a0ce982a6017d70111
tomatizuo-dot <tomatizuo@gmail.com>
2026-06-10 00:28:02 +0900

    Update Checkbox.lua

--- src/UI/Checkbox.lua
+++ src/UI/Checkbox.lua
@@ -1,1 +1,25 @@
+local Checkbox = {}
+Checkbox.__index = Checkbox
 
+function Checkbox.new(Default)
+	local Self = setmetatable({}, Checkbox)
+
+	Self.Value = Default or false
+
+	return Self
+end
+
+function Checkbox:Set(Value)
+	self.Value = Value
+end
+
+function Checkbox:Get()
+	return self.Value
+end
+
+function Checkbox:Toggle()
+	self.Value = not self.Value
+	return self.Value
+end
+
+return Checkbox
