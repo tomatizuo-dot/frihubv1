ce973508d80de231aa5fb5a64741c19b9ab08bc2
tomatizuo-dot <tomatizuo@gmail.com>
2026-06-10 00:14:03 +0900

    Create Renderer.lua

---
+++ src/UI/Renderer.lua
@@ -0,0 +1,23 @@
+local Renderer = {}
+
+function Renderer:CreateWindow(Category)
+	-- Combatウィンドウ生成
+end
+
+function Renderer:CreateModule(ModuleData)
+	-- TriggerBot生成
+end
+
+function Renderer:CreateToggle(Name, Value)
+	-- Toggle生成
+end
+
+function Renderer:CreateSlider(Name, Slider)
+	-- Slider生成
+end
+
+function Renderer:CreateDropdown(Name, Dropdown)
+	-- Dropdown生成
+end
+
+return Renderer
