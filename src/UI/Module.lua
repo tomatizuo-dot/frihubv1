b7faa2d899af2a13371ae3e61e3f2a314230cc69
tomatizuo-dot <tomatizuo@gmail.com>
2026-06-10 00:06:13 +0900

    Update Module.lua

--- src/UI/Module.lua
+++ src/UI/Module.lua
@@ -1,13 +1,27 @@
 local Module = {}
+Module.__index = Module
 
 function Module.new(Name)
-    local Self = {}
+    local Self = setmetatable({}, Module)
 
     Self.Name = Name
     Self.Enabled = false
     Self.Expanded = false
+    Self.Settings = {}
 
     return Self
+end
+
+function Module:Toggle()
+    self.Enabled = not self.Enabled
+end
+
+function Module:SetExpanded(State)
+    self.Expanded = State
+end
+
+function Module:AddSetting(Name, Default)
+    self.Settings[Name] = Default
 end
 
 return Module
