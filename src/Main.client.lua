fd53176b8f4018e5a6445dc29497105c0153f3d2
tomatizuo-dot <tomatizuo@gmail.com>
2026-06-10 00:30:46 +0900

    Update Main.client.lua

--- src/Main.client.lua
+++ src/Main.client.lua
@@ -11,3 +11,15 @@ local Categories = {
 local Windows = Renderer:Render(Categories)
 
 print("Loaded Windows:", #Windows)
+
+for _, Window in ipairs(Windows) do
+	print(Window.Name)
+
+	for _, Module in ipairs(Window:GetModules()) do
+		print("  ->", Module.Name)
+
+		for SettingName, SettingValue in pairs(Module:GetSettings()) do
+			print("     ", SettingName, SettingValue)
+		end
+	end
+end
