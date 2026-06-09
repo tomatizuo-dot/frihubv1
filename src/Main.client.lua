bd3e287385b94b06b72d56ccdec0c91203fa58cb
tomatizuo-dot <tomatizuo@gmail.com>
2026-06-10 00:04:22 +0900

    Update Main.client.lua

--- src/Main.client.lua
+++ src/Main.client.lua
@@ -1,1 +1,11 @@
+local Categories = {
+    require(script.Parent.Categories.Combat),
+    require(script.Parent.Categories.Movement),
+    require(script.Parent.Categories.Render),
+    require(script.Parent.Categories.Misc),
+    require(script.Parent.Categories.Settings)
+}
 
+for _, Category in ipairs(Categories) do
+    print("Loading:", Category.Name)
+end
