f0c24b3771e264d4792c8d99cea2c9f23c569190
tomatizuo-dot <tomatizuo@gmail.com>
2026-06-10 00:21:56 +0900

    Update Module.lua

--- src/UI/Module.lua
+++ src/UI/Module.lua
@@ -24,5 +24,20 @@ end
 function Module:GetSetting(Name)
 	return self.Settings[Name]
 end
+function Module:GetSettings()
+	return self.Settings
+end
+
+function Module:SetExpanded(State)
+	self.Expanded = State
+end
+
+function Module:IsExpanded()
+	return self.Expanded
+end
+
+function Module:IsEnabled()
+	return self.Enabled
+end
 
 return Module
